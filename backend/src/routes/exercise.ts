13f992cad51f301fd5966b6bc78b4f2c8f5264a7
ejsadiarin <ejsadiarin@gmail.com>
2023-12-07 23:25:56 +0800

    added necessary endpoints

--- backend/src/routes/exercise.ts
+++ backend/src/routes/exercise.ts
@@ -2,15 +2,28 @@ import { Router } from 'express';
 
 export const router = Router();
 
+// Get all exercises
 router.get('/', (req, res) => {
   console.log('test');
   res.send(`Hey ${req.eventNames}`)
 })
 
+// Get specific exercise
 router.get('/:id', (req, res) => {
   res.send(`Hey ${req.params.id}`);
 })
 
+// Add new exercise
 router.post('/', (req, res) => {
+  res.send('test');
+})
+
+// Update exercise
+router.put('/:id', (req, res) => {
+  res.send('test');
+})
+
+// Delete exercise
+router.delete('/:id', (req, res) => {
   res.send('test');
 })
--- backend/src/routes/user.ts
+++ backend/src/routes/user.ts
@@ -1,1 +1,18 @@
-console.log('test');
+import { Router } from 'express';
+
+const router = Router();
+
+// Get specific user
+router.get('/:id', (req, res) => {
+  res.send('test');
+});
+
+// Add new user
+router.post('/', (req, res) => {
+  res.send('test');
+});
+
+// Update specific user details
+router.put('/:id', (req, res) => {
+  res.send('test');
+});
